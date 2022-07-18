c73fe04cafac0a4cf7c3fd8a670e3d8a92090f3c
LuisRG98 <70450227+LuisRG98@users.noreply.github.com>
2022-07-18 09:37:06 -0400

    Update implement_strstr().py
    Co-authored-by: ReddyTintaya  <reddy.tintaya.dev@gmail.com>

--- implement_strstr().py
+++ implement_strstr().py
@@ -1,6 +1,5 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle in haystack:
-            return(haystack.index(needle))
-        else:
-            return -1
+            return haystack.index(needle)
+        return -1
